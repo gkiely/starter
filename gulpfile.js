95b353ec1b26ee01fb9d799de0f1e96f55934ae0
Grant <grant.kiely@gmail.com>
2015-11-05 21:31:43 +1100

    rm eslint for time being

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var babel         = require('gulp-babel'),
     bulkSass      = require('gulp-sass-bulk-import'),
     cache         = require('gulp-cached'),
     concat        = require('gulp-concat'),
-    eslint        = require('gulp-eslint'),
     fileInclude   = require('gulp-file-include'),
     gulp          = require('gulp'),
     livereload    = require('gulp-livereload'),
@@ -83,7 +82,7 @@ gulp.task('js', function(){
   .pipe(babel())
   .pipe(ngAnnotate())
   .pipe(remember('scripts'))
-  .pipe(eslint.format())
+  // .pipe(eslint.format())``
   .pipe(concat('app.js'))
   // .pipe(sourcemaps.write('.'))
   .on('error', handleError)
