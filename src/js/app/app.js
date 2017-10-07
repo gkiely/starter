986ffb892bf9e69d68772b034b191e69576794c8
Grant <grant.kiely@gmail.com>
2017-10-07 13:51:27 +1100

    updated polyfill reference

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,26 +1,23 @@
 /*=====================================
 =            External Deps            =
 =====================================*/
+import 'util/polyfills';
 /*=====  End of External Deps  ======*/
-
-
 
 /*=================================
 =             App code            =
 =================================*/
-let gup               = require('util/gup');
-let {log}             = require('util/helpers');
+// let gup               = require('util/gup');
+// let {log}             = require('util/helpers');
 /*=====  End of Custom app code  ======*/
 
 
 /*===============================
 =            Helpers            =
 ===============================*/
-let getPath = function(str){
-  return str.replace(/^\//, '');
-};
-
-
+// let getPath = function(str){
+//   return str.replace(/^\//, '');
+// };
 /*=====  End of Helpers  ======*/
 
 
@@ -28,7 +25,7 @@ let getPath = function(str){
 /*=============================
 =            Setup            =
 =============================*/
-let host    = window.location.hostname;
+// let host    = window.location.hostname;
 /*=====  End of Setup  ======*/
 
 
