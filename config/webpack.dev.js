685785c4ce07c4bb927a68991fee15aa2e3bba2d
Grant <grant.kiely@gmail.com>
2017-10-17 06:28:41 +1100

    updates

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -24,7 +24,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: 'babel-loader?cacheDirectory',
         exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react']
